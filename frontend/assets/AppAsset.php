a99700d727c7792001eac7f54647824ac6ae4778
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2016-06-03 05:34:09 +0000

    yii

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -20,6 +20,7 @@ class AppAsset extends AssetBundle
     public $css = [
         'css/site.css',
         'css/main.css',
+        'css/common.css',
     ];
     public $js = [
         'js/core.js',
