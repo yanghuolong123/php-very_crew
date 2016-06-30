91473db8754014cb4139878060d90ec69caa99c2
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2016-06-30 10:22:01 +0000

    yii

--- frontend/views/user/view.php
+++ frontend/views/user/view.php
@@ -80,9 +80,9 @@ $this->registerJsFile('@web/js/main.js',['depends'=>['app\assets\AppAsset']]);
         <div class="row">
             <div class="container"><h3>个人作品</h3></div>
             <?php foreach ($perVideo as $video): ?>
-            <div class="col-sm-6 col-md-4">
+            <div class="col-sm-6 col-md-3">
               <div class="thumbnail">
-                  <a href="<?= Url::to(['video/view', 'id'=>$video->id]) ?>"><img style="height:350px; width:350px;" src="<?= $video->logo ?>" alt="<?= $video->title ?>"></a>
+                  <a href="<?= Url::to(['video/view', 'id'=>$video->id]) ?>"><img src="<?= $video->logo ?>" alt="<?= $video->title ?>"></a>
                 <div class="caption">
                     <h3><a href="<?= Url::to(['video/view', 'id'=>$video->id]) ?>"><?= $video->title ?></a></h3>
                   <p><?= MetaData::getVal($video->type) ?>  - <?= implode(', ',MetaData::getArrVal(explode(',', trim($video->tag)))) ?></p>  
