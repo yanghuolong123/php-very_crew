033211e5b41dff357f595aaad5567389656e0254
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2016-07-01 10:08:30 +0000

    yii

--- frontend/views/game/index.php
+++ frontend/views/game/index.php
@@ -1,42 +1,4 @@
-<?php
-
-use yii\helpers\Html;
-use yii\widgets\ActiveForm;
-use app\models\extend\MetaData;
-
-$this->registerJsFile('@web/js/main.js',['depends'=>['app\assets\AppAsset']]);  
-
-$planList = \app\models\extend\Plan::getPlanList(Yii::$app->user->id);
-?>
-
-<div class="video-form">
-
-    <?php $form = ActiveForm::begin([
-        'id' => 'video-form',
-        'options' => ['enctype' => 'multipart/form-data','class' => 'form-horizontal', 'onsubmit'=>''],
-        'fieldClass' => 'app\util\ExtActiveField',
-        'fieldConfig' => [
-            'template' => "{label}\n<div class=\"col-lg-4\">{input}</div>\n<div class=\"col-lg-5\">{error}</div>",
-            'labelOptions' => ['class' => 'col-lg-2 control-label'],
-        ],
-    ]); ?>    
-
-    
-    <?= $form->field($model, 'file')->fileInput() ?>   
-    <?= $form->field($model, 'file')->imgInput() ?>
-    
-    <input type="file"  id="picupload-dsdssdds" accept="image/png,image/gif,image/jpg,image/jpeg">
-    
-    <input type="file"  id="picupload-dsdssdds" accept=".jpg,.png,.jpeg,.gif">
-
-    <div class="form-group">
-        <div class="col-sm-1 col-md-offset-2">
-        <?= Html::submitButton('提交并保存', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-        </div>
-    </div>
-
-    <?php ActiveForm::end(); ?>
-
+<div class="alert alert-info">
+    <h2>功能开发中，敬请期待。。。。</h2>
 </div>
-
 
