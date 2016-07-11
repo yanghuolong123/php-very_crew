ef5b9ae221bf872a05b0cd6bdfaf58dff24a5897
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2016-07-11 07:51:55 +0000

    yii

--- frontend/views/plan/my.php
+++ frontend/views/plan/my.php
@@ -14,7 +14,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php //echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('发布我的计划', ['create'], ['class' => 'btn btn-success']) ?>
+        <?php // Html::a('发布我的计划', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
     <?= GridView::widget([
         //'showHeader' => false,
