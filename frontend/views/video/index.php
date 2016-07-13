cb3481bf039ff454c4dc244661ae1d102bfa0b0c
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2016-07-13 08:10:17 +0000

    yii

--- frontend/views/video/index.php
+++ frontend/views/video/index.php
@@ -19,8 +19,8 @@ $this->params['breadcrumbs'][] = $this->title;
           <div class="thumbnail">
             <a href="<?= Url::to(['video/view', 'id'=>$video->id]) ?>"><img src="<?= $video->logo ?>" alt="<?= $video->title ?>"></a>
             <div class="caption">
-                <h3><a href="<?= Url::to(['video/view', 'id'=>$video->id]) ?>"><?= $video->title ?></a></h3>
-              <p><?= MetaData::getVal($video->type) ?>  - <?= implode(', ',MetaData::getArrVal(explode(',', trim($video->tag)))) ?></p>  
+              <h3><a href="<?= Url::to(['video/view', 'id'=>$video->id]) ?>"><?= $video->title ?></a></h3>
+              <p><?= MetaData::getVal($video->type) ?>  - <?= CommonUtil::cutstr(implode(', ',MetaData::getArrVal(explode(',', trim($video->tag)))), 26) ?></p>  
               <p><?= $video->views ?>人气/ <?= $video->comments ?>点评/ <?= $video->support ?>赞</p>
             </div>
           </div>
