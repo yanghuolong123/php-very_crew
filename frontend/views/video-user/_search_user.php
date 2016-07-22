18f5368fced0f9f5970ec1276df41a0d71b52857
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2016-07-22 06:34:59 +0000

    yii

--- frontend/views/video-user/_search_user.php
+++ frontend/views/video-user/_search_user.php
@@ -8,7 +8,8 @@ use yii\helpers\Url;
 <div class="user-search">
 
     <?php $form = ActiveForm::begin([
-//        'action' => ['index'],
+        'id' => 'search_user_video',
+        'action' => ['user-search'],
 //        'method' => 'get',
         'options' => ['class' => 'form-horizontal'],
 //        'fieldConfig' => [
@@ -19,12 +20,13 @@ use yii\helpers\Url;
 
     <div class="form-group field-usersearch-nickname">
         <label for="usersearch" class="col-lg-2 control-label">姓名/ID</label>
-        <div class="col-lg-4"><input type="text" name="userSearch" class="form-control" id="usersearch"></div>        
+        <div class="col-lg-4"><input type="text" name="search" class="form-control" id="usersearch"></div>        
     </div>
     
     <div class="form-group">
         <div class="col-sm-1 col-md-offset-2 col-md-2">
-        <?= Html::button('搜索', ['class' => 'btn btn-primary user-search-bt']) ?>
+        <?= Html::hiddenInput('video_id', $_GET['video_id']) ?>
+        <?= Html::submitButton('搜索', ['class' => 'btn btn-primary']) ?>
         <?= Html::resetButton('重置', ['class' => 'btn btn-default']) ?>
         </div>
     </div>
@@ -36,13 +38,22 @@ use yii\helpers\Url;
 
 <?php $this->beginBlock('userSearchJs') ?> 
 
-$(function() {    
-    $(".user-search-bt").click(function(){
-        var search = $('#usersearch').val();
-        $.post("<?= Url::to(['user-search']) ?>",{search:search, video_id:<?= $_GET['video_id'] ?>}, function(data){
-            $('#search-user-content').html(data);
+$(function() {  
+    $('#search_user_video').on('beforeSubmit', function (e) {
+        var $form = $(this);
+        $.ajax({
+            url: $form.attr('action'),
+            type: 'post',
+            data: $form.serialize(),
+            success: function (data) {
+                // do something
+                $('#search-user-content').html(data);
+            }
         });
+    }).on('submit', function (e) {
+        e.preventDefault();
     });
+    
 });
 
 <?php $this->endBlock() ?> 
