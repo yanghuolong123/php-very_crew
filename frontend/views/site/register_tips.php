6598c411c2d7a7006cb28e5aa20470a4d5fb114f
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2016-08-01 07:43:31 +0000

    yii

--- frontend/views/site/register_tips.php
+++ frontend/views/site/register_tips.php
@@ -1,6 +1,6 @@
 <?php
 
-use Yii;
+//use Yii;
 use yii\helpers\Url; 
 
 $this->title = '消息提醒';
@@ -10,6 +10,6 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <div class="alert alert-success">
     <h4>注册成功！</h4>
-    <p>为了大家更好地了解你，并获得更多合作机会，建议您尽快在 <a href="<?= Url::to(['user/view', 'id'=>Yii::$app->user->id]) ?>">“我的资料”</a> 页面完善个人信息</p>
+    <p>为了大家更好地了解你，并获得更多合作机会，建议您尽快在 <a href="<?= Url::to(['user-profile/update', 'uid'=>Yii::$app->user->id]) ?>">“完善我的资料”</a> 页面完善个人信息</p>
     <p><a class="btn btn-primary" href="<?= Url::to(['user-profile/update', 'uid'=>Yii::$app->user->id]) ?>">现在就去 &raquo;</a></p>    
 </div>
