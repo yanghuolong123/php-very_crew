98af4c7d4d7ec1656b45e114ed1fc7eb523e8d22
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2016-08-03 11:27:46 +0000

    yii

--- frontend/views/plan/index.php
+++ frontend/views/plan/index.php
@@ -21,11 +21,11 @@ $this->params['breadcrumbs'][] = $this->title;
           <div class="thumbnail" style="height:272px;">
 
             <div class="caption">
-                <h3><a href="<?= Url::to(['plan/view', 'id'=>$plan->id]) ?>"><?= $plan->title ?></a></h3>
+                <h3><a href="<?= Url::to(['plan/view', 'id'=>$plan->id]) ?>"><?= CommonUtil::cutstr($plan->title,24) ?></a></h3>
                 <p><?= MetaData::getVal($plan->type) ?>  - <?= CommonUtil::cutstr(implode(', ',MetaData::getArrVal(explode(',', trim($plan->tag)))),40) ?></p>  
                 <p>拍摄地区：<?= implode(' ',Distrinct::getArrDistrict([$plan->province, $plan->city, $plan->county])) ?></p>
                 <p>所需角色：<?= CommonUtil::cutstr(implode(', ',MetaData::getArrVal(explode(',', trim($plan->plan_role)))), 40) ?></p>
-                <p>所需演技：<?= CommonUtil::cutstr(implode(', ',MetaData::getArrVal(explode(',', trim($plan->plan_skill)))), 40) ?></p>
+                <p>所需演技：<?= CommonUtil::cutstr(implode(', ',MetaData::getArrVal(explode(',', trim($plan->plan_skill)))), 38) ?></p>
                 <p>剧情简介：<?= CommonUtil::cutstr($plan->content, 125) ?></p>
             </div>
           </div>
