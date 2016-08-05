8eec71602e860711cb789fd4c2f97f009a509e8a
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2016-08-05 14:06:44 +0000

    yii

--- frontend/models/extend/UserProfile.php
+++ frontend/models/extend/UserProfile.php
@@ -8,7 +8,7 @@ class UserProfile extends \app\models\native\TblUserProfile {
 
     public function rules() {
         return [
-            [['gender', 'birthday', 'province', 'city', 'county', 'good_at_job', 'speciality', 'usingtime'], 'required'],
+            [['gender', 'birthday', 'province', 'city', 'county', 'good_at_job', 'speciality'], 'required'],
             [['id', 'uid', 'gender', 'province', 'city', 'county', 'country'], 'integer'],
             [['birthday', 'weixin', 'qq'], 'string', 'max' => 65],
             [['height', 'weight'], 'string', 'max' => 25],
@@ -20,7 +20,7 @@ class UserProfile extends \app\models\native\TblUserProfile {
     public function beforeValidate() {
         $this->good_at_job = is_array($this->good_at_job) ? ',' . implode(',', $this->good_at_job) . ',' : $this->good_at_job;
         $this->speciality = is_array($this->speciality) ? ',' . implode(',', $this->speciality) . ',' : $this->speciality;
-        $this->usingtime = is_array($this->usingtime) ? ',' . implode(',', $this->usingtime) . ',' : $this->usingtime;
+       // $this->usingtime = is_array($this->usingtime) ? ',' . implode(',', $this->usingtime) . ',' : $this->usingtime;
 
         return parent::beforeValidate();
     }
