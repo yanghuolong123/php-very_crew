bb6ff1a6bd38bdedd0f909becd8892931985bba6
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2016-08-05 14:23:48 +0000

    yii

--- frontend/views/user-profile/update.php
+++ frontend/views/user-profile/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 
 
 $this->title = '编辑个人资料: ' . $model->id;
-$this->params['breadcrumbs'][] = ['label' => '个人资料', 'url' => ['view', 'uid' => $model->uid]];
+$this->params['breadcrumbs'][] = ['label' => '个人资料', 'url' => ['user/view', 'id' => $model->uid]];
 $this->params['breadcrumbs'][] = '编辑';
 ?>
 <div class="user-profile-update">
