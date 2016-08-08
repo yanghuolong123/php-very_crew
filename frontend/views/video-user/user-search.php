51b8d4b459116312aa116b40e5faece9d82a81a5
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2016-08-08 09:28:03 +0000

    yii

--- frontend/views/video-user/user-search.php
+++ frontend/views/video-user/user-search.php
@@ -2,6 +2,7 @@
 use app\models\extend\Distrinct;
 use app\models\extend\User;
 use yii\helpers\Url;
+use app\util\CommonUtil;
 ?>
 <?php if(empty($userModel)): ?>
 <div class="alert alert-info">没有搜索到匹配的成员</div>
@@ -11,9 +12,9 @@ use yii\helpers\Url;
     <?php foreach ($userModel as $user): ?>
     <div class="col-sm-6 col-md-3">
       <div class="thumbnail">
-          <a href="<?= Url::to(['user/view', 'id'=>$user->id]) ?>"><img src="<?= User::getInfo($user->id)->avatar.'!250!250' ?>" alt="<?= User::getInfo($user->id)->nickname ?>"></a>
+          <a href="<?= Url::to(['user/view', 'id'=>$user->id]) ?>"><img src="<?= CommonUtil::cropImgLink(User::getInfo($user->id)->avatar,250,250) ?>" alt="<?= User::getInfo($user->id)->nickname ?>"></a>
         <div class="caption">
-          <h3><a href="<?= Url::to(['user/view', 'id'=>$user->id]) ?>"><?= User::getInfo($user->id)->nickname ?></a></h3>
+          <h4><a href="<?= Url::to(['user/view', 'id'=>$user->id]) ?>"><?= User::getInfo($user->id)->nickname ?></a></h4>
           <p><?= implode(' ',Distrinct::getArrDistrict([$user->profile['province'], $user->profile['city'], $user->profile['county'], $user->profile['country']])) ?></p>              
           <p><a class="btn btn-default" href="<?= Url::to(['video-user/create', 'uid'=>$user->id, 'video_id'=>$video_id]) ?>">添加他为我的作品成员 &raquo;</a></p>
         </div>
