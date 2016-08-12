1525c6041244128850237dabb1d742fe4efd5391
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2016-08-12 04:35:30 +0000

    yii

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -11,7 +11,7 @@ $this->title = '非常剧组-后台管理系统';
 
         <p class="lead">最大的风险 就是不冒险  世界变化如此快速   唯一不让我们肯定失败的就是不敢去冒险~~~。</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://my.frontend.verycrew.com">进入前台</a></p>
+        <p><a class="btn btn-lg btn-success" href="http://www.feichangjuzu.com">进入前台</a></p>
     </div>
 
     <div class="body-content">
