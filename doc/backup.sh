3931a036707e1f767c7171876764b15296dfe4b7
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2016-08-13 10:13:41 +0000

    yii

--- doc/backup.sh
+++ doc/backup.sh
@@ -4,3 +4,15 @@ cd /home/work/backup/sqldata
 subfix=`date "+%Y-%m-%d_%H%M%S"`
 mysqldump -u feichangjuzu -pyhl-feichangjuzu zd_very_crew --skip-lock-tables > zd_very_crew_"$subfix".sql
 
+USER="feichangjuzu"
+PASS="yhl-feichangjuzu"
+
+mysql -u $USER -p$PASS <<EOF 2> /dev/null
+DROP DATABASE zd_very_crew-test;
+create database zd_very_crew-test default character set utf8;
+EOF
+
+mysql -u $USER -p$PASS zd_very_crew-test <<EOF 2> /dev/null
+DROP DATABASE zd_very_crew-test;
+source zd_very_crew_"$subfix".sql
+EOF
