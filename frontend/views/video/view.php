53b43420f4e70e11bc0cbacd8eb88a9dc0afa0fe
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2016-08-16 06:47:48 +0000

    yii

--- frontend/views/video/view.php
+++ frontend/views/video/view.php
@@ -89,7 +89,7 @@ if (!CommonUtil::isMobile()) {
                     <?php foreach ($members as $user): ?>
                     <div class="col-sm-6 col-md-2">
                       <div class="thumbnail">
-                        <a href="<?= Url::to(['user/view', 'id'=>$user->uid]) ?>"><img src="<?= CommonUtil::cropImgLink(User::getInfo($user->uid)->avatar, 160, 160) ?>" alt="<?= User::getInfo($user->uid)->nickname ?>"></a>
+                        <a href="<?= Url::to(['user/view', 'id'=>$user->uid]) ?>"><img src="<?= CommonUtil::cropImgLink(User::getInfo($user->uid)->avatar, 320, 320) ?>" alt="<?= User::getInfo($user->uid)->nickname ?>"></a>
                         <div class="caption">
                           <h4><a href="<?= Url::to(['user/view', 'id'=>$user->uid]) ?>"><?= User::getInfo($user->uid)->nickname ?></a></h4>
                           <p>角色：<?php if(empty($user->type)): ?>发起人 <?php endif; ?><?= CommonUtil::cutstr($user->role_name, 5) ?></p>              
