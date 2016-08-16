79368548bb6a4d9b7366bbde559eaed48ff6920f
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2016-08-16 06:43:44 +0000

    yii

--- frontend/views/home/index.php
+++ frontend/views/home/index.php
@@ -77,9 +77,9 @@ $this->title = '非常剧组';
 <div class="row">
     <div class="container"><h3>搭档推荐</h3></div>
     <?php foreach ($recomUsers as $user): ?>
-    <div class="col-sm-2 col-md-2">
+    <div class="col-sm-6 col-md-2">
       <div class="thumbnail">
-         <a href="<?= Url::to(['user/view', 'id'=>$user->id]) ?>"><img src="<?= CommonUtil::cropImgLink(User::getInfo($user->id)->avatar, 160, 160) ?>" alt="<?= User::getInfo($user->id)->nickname ?>"></a>
+         <a href="<?= Url::to(['user/view', 'id'=>$user->id]) ?>"><img src="<?= CommonUtil::cropImgLink(User::getInfo($user->id)->avatar, 320, 320) ?>" alt="<?= User::getInfo($user->id)->nickname ?>"></a>
         <div class="caption">
           <h4><a href="<?= Url::to(['user/view', 'id'=>$user->id]) ?>"><?= User::getInfo($user->id)->nickname ?></a></h4>
           <p><?php // implode(' ',Distrinct::getArrDistrict([$user->profile['province'], $user->profile['city'], $user->profile['county'], $user->profile['country']])) ?></p>              
