6e72863e96f495a84db1161365eae4238ed2db58
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2016-10-04 03:32:48 +0000

    yii

--- backend/config/params.php
+++ backend/config/params.php
@@ -1,5 +1,5 @@
 <?php
-var_dump(YII_ENV_DEV);
+
 return [
     'adminEmail' => 'admin@example.com',
     'frontendHost' => YII_ENV_DEV ? 'http://my.frontend.verycrew.com' : 'http://feichangjuzu.com/',
