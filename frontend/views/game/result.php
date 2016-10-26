75646dd9abb2de4ea3c7a06ec1c5bef325a5dd80
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2016-10-26 02:44:42 +0000

    yii

--- frontend/views/game/result.php
+++ frontend/views/game/result.php
@@ -6,5 +6,11 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 
 <div class="row">
+    <?php if(!empty($model->result)): ?>
     <div class="col-md-12"><p><?= $model->result ?></p></div>
+    <?php else: ?>
+    <div class="alert alert-info">
+        <h3>比赛结果还为揭晓...</h3>
+    </div>
+    <?php endif; ?>
 </div>
