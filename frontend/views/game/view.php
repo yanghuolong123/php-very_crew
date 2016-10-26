5ffade8b51b5a1f9cd91b9c59f43c2a7346229eb
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2016-10-26 05:32:05 +0000

    yii

--- frontend/views/game/view.php
+++ frontend/views/game/view.php
@@ -61,7 +61,7 @@ $this->params['breadcrumbs'][] = $this->title;
             <div class="caption">
               <h4><a data-toggle="tooltip" data-placement="bottom" title="<?= $video->title ?>" href="<?= Url::to(['video/view', 'id'=>$video->id]) ?>"><?= CommonUtil::cutstr($video->title,22) ?></a></h4>
               <p>ID：<?= $gameVideo->id ?></p>  
-              <p>作品得分：<span class="gamenum"><?= $gameVideo->score ?></span></p>
+              <p>作品得分：<span><?= empty($gameVideo->score) ? '--' : $gameVideo->score;  ?></span></p>
               <p>当前票数：<span class="gamenum" id="votes_<?= $gameVideo->id ?>"><?= $gameVideo->votes ?></span></p>
               <p class="text-left"><button <?php if($model->status!=1): ?>disabled="disabled"<?php endif; ?> type="button" onclick="gameVote(<?= $gameVideo->id ?>)" class="btn btn-primary btn-small game_vote">投一票</button></p>
             </div>
