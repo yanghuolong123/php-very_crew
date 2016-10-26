cc8a48df5d95b2a0d9f9ace442b296d0287f6331
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2016-10-26 02:52:05 +0000

    yii

--- backend/models/extend/Games.php
+++ backend/models/extend/Games.php
@@ -32,6 +32,8 @@ class Games extends \app\models\native\TblGames {
         $arr = [
             0 => '作品上传进行中',
             1 => '投票进行中',
+            2 => '大赛结果评比中',
+            2 => '大赛已结束',
         ];
 
         return $isArr ? $arr : (isset($arr[$status]) ? $arr[$status] : '');
