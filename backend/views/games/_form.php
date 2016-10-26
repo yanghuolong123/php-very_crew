e19fe28666d77a99b718a7052d7497816cdb6358
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2016-10-26 02:30:44 +0000

    yii

--- backend/views/games/_form.php
+++ backend/views/games/_form.php
@@ -77,7 +77,27 @@ $this->registerJsFile('@web/js/main.js',['depends'=>['app\assets\AppAsset']]);
     ]);
     ?>  
     
-    <?= $form->field($model, 'result')->textarea() ?>
+    <?php // $form->field($model, 'result')->textarea() ?>
+    <?= $form->field($model,'result')->widget('kucha\ueditor\UEditor',[
+            'clientOptions' => [
+                //编辑区域大小
+                'initialFrameHeight' => '200',
+                //设置语言
+                'lang' =>'zh-cn', //中文为 zh-cn
+                //定制菜单
+//                'toolbars' => [
+//                    [
+//                        'fullscreen', 'source', 'undo', 'redo', '|',
+//                        'fontsize',
+//                        'bold', 'italic', 'underline', 'fontborder', 'strikethrough', 'removeformat',
+//                        'formatmatch', 'autotypeset', 'blockquote', 'pasteplain', '|',
+//                        'forecolor', 'backcolor', '|',
+//                        'lineheight', '|',
+//                        'indent', '|'
+//                    ],
+//                ],
+            ]
+    ]); ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
