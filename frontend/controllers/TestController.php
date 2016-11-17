115af39705277d8938ecbd14c2c8a1fb8067fdf8
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2016-11-17 05:45:48 +0000

    yii

--- frontend/controllers/TestController.php
+++ frontend/controllers/TestController.php
@@ -3,6 +3,8 @@
 namespace app\controllers;
 
 use Yii;
+use yii\helpers\Html;
+use yii\helpers\Url;
 
 class TestController extends \app\util\BaseController {
 
@@ -18,12 +20,19 @@ class TestController extends \app\util\BaseController {
 //        //$collection->insert(['name' => 'John Smith', 'status' => 1]);
 //        $data = $collection->findOne(['name' => 'John Smith', 'status' => 1]); 
         
+//        Yii::$app->mailer->compose()
+//                ->setTo('yanghuolong@zhisland.com')
+//                ->setFrom(['yhl27ml@163.com' => 'jason'])
+//                ->setSubject("mailer邮件测试22")
+//                ->setTextBody('Hello World!')
+//                ->send();
+        
         Yii::$app->mailer->compose()
-                ->setTo('yanghuolong@zhisland.com')
-                ->setFrom(['yhl27ml@163.com' => 'jason'])
-                ->setSubject("mailer邮件测试22")
-                ->setTextBody('Hello World!')
-                ->send();
+                    ->setTo('yanghuolong@zhisland.com')
+                    ->setFrom(['yhl27ml@163.com' => '非常剧组'])
+                    ->setSubject("非常剧组大赛投票")
+                    ->setHtmlBody('欢迎您参加大赛投票，' . Html::a('进行投票', Url::to(['game/email-vote', 'email'=>'yanghuolong@zhisland.com', 'gameVideoId' => 1])))
+                    ->send();
     }
 
 }
