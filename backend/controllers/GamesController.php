e39e6a9365c5d9132561ecbdff4e70b729aba664
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2017-01-01 04:20:51 +0000

    yii

--- backend/controllers/GamesController.php
+++ backend/controllers/GamesController.php
@@ -8,6 +8,7 @@ use app\models\search\GamesSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
 
 /**
  * GamesController implements the CRUD actions for Games model.
@@ -20,6 +21,15 @@ class GamesController extends Controller
     public function behaviors()
     {
         return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
