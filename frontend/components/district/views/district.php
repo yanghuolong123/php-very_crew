15f47328713bfa345d34c682ce6a3c18ae9da45a
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2017-01-17 06:26:18 +0000

    yii

--- frontend/components/district/views/district.php
+++ frontend/components/district/views/district.php
@@ -8,7 +8,7 @@ $ismobile = false;
 $optionClass = "form-horizontal";
 if (CommonUtil::isMobile()) {
     $ismobile = true;
-    $optionClass = "form-control";
+    $optionClass = "form-group";
 }
 ?>
 <style>
@@ -20,7 +20,7 @@ if (CommonUtil::isMobile()) {
 
 $form->field($model, 'province', [
     'template' => "{label}\n<div class=\"col-lg-2\" style=\"padding-left: 5px;margin-bottom: 5px;\">{input}</div>\n",
-    'options' => ['class' => 'form-horizontal'],
+    'options' => ['class' => $optionClass],
     'labelOptions' => ['class' => 'col-lg-2 control-label district-label'],
 ])->dropDownList(Distrinct::getDistrictList(0), [
     'prompt' => '请选择省',
@@ -38,7 +38,7 @@ $form->field($model, 'province', [
 
 $form->field($model, 'city', [
     'template' => "<div class=\"col-lg-2\" style=\"margin-bottom: 5px;\">{input}</div>\n",
-    'options' => ['class' => 'form-horizontal'],
+    'options' => ['class' => $optionClass],
 ])->dropDownList(Distrinct::getDistrictList($model->province), [
     'prompt' => '请选择城市',
     'onchange' => '
@@ -65,7 +65,7 @@ $form->field($model, 'city', [
 
     $form->field($model, 'county', [
         'template' => "<div class=\"col-lg-2\">{input}</div>\n",
-        'options' => ['class' => 'form-horizontal'],
+        'options' => ['class' => $optionClass],
     ])->dropDownList(Distrinct::getDistrictList($model->city), [
         'prompt' => '请选择县',
         'onchange' => '
