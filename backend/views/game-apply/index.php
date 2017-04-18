02d030620c851bff55759eae48779710aa224a43
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2017-04-18 07:23:16 +0000

    yii

--- backend/views/game-apply/index.php
+++ backend/views/game-apply/index.php
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Game Apply', ['create'], ['class' => 'btn btn-success']) ?>
+        <?php // Html::a('Create Game Apply', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
