4da162d365acfc0674757722e66a3c1ebc83076b
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2017-05-14 01:32:25 +0000

    yii

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -26,7 +26,7 @@ $footerNav = Article::getByGroopKey('footer_nav');
 <html lang="<?= Yii::$app->language ?>">
     <head>
         <meta charset="<?= Yii::$app->charset ?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <?= Html::csrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
