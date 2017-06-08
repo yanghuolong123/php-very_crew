118b8a334fb2e3ed9117015eacedd9594ad71a84
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2017-06-08 11:39:55 +0000

    yii

--- frontend/modules/weixin/models/JSSDK.php
+++ frontend/modules/weixin/models/JSSDK.php
@@ -65,6 +65,9 @@ class JSSDK {
 
         $url = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token=$this->accessToken";
         $res = json_decode(HttpUtil::curl_get($url), true);
+        echo '<pre>';
+        var_dump($res);
+        echo '</pre>';
         $ticket = $res['ticket'];
         if ($ticket) {
             $cache->set("jsapi_ticket_" . $this->appId, $ticket, 3600);
