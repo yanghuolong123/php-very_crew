3aa35139502b5c05341680cc71fd5d110b76c86b
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2017-06-10 08:27:52 +0000

    yii

--- frontend/views/video/view.php
+++ frontend/views/video/view.php
@@ -163,7 +163,7 @@ var player = videojs('payer', options, function onPlayerReady() {
 
 <?=
 $this->render('/inc/_wxshare', [
-    'title' => '非常剧组-《'.trim($model->title,'《..》').'》',
+    'title' => '非常剧组-《'.$model->title.'》',
     'content' => $model->content,
     'logo' => $model->logo,
     'shareUrl' => CURRENTURL,
