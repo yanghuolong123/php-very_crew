64fc77496f9120e58600d627515265523b973593
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2017-06-22 10:49:44 +0000

    yii

--- frontend/models/extend/GameVideo.php
+++ frontend/models/extend/GameVideo.php
@@ -6,23 +6,22 @@ use Yii;
 use app\models\extend\Video;
 
 class GameVideo extends \app\models\native\TblGameVideo {
-    
+
     public static function gameVote($videoId, $openId) {
         $videoModel = Video::findOne($videoId);
-        if(empty($videoId)) {
+        if (empty($videoId)) {
             return;
         }
-        
+
         $collection = Yii::$app->mongodb->getCollection('game_vote_record');
         if (!empty($collection->findOne(['game_video_id' => $videoId, 'open_id' => $openId]))) {
-            return '亲，你已经对作品 《'.$videoModel->title.'》投票过了，感谢你的参与!';
+            return '亲，你已经对作品 《' . $videoModel->title . '》投票过了，感谢你的参与!';
         }
-        
-        $model = $this->findOne(['video_id'=>$videoId]);
-        $model->updateCounters(['votes' => 1]);
-        $collection->insert(['game_video_id' => $model->id, 'open_id' => $openId, 'time' => DATETIME]);
-        
-        return '亲，感谢你对作品 《'.$videoModel->title.'》投票了一票!';
+
+        self::updateAllCounters(['votes' => 1], ['video_id' => $videoId]);
+        $collection->insert(['game_video_id' => $videoId, 'open_id' => $openId, 'time' => DATETIME]);
+
+        return '亲，感谢你对作品 《' . $videoModel->title . '》投票了一票!';
     }
-    
+
 }
