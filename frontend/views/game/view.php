a9c1a675bb53769fa15609ec90cce78626619326
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2017-06-23 09:42:24 +0000

    yii

--- frontend/views/game/view.php
+++ frontend/views/game/view.php
@@ -126,7 +126,7 @@ $(function(){
             }
 
             var imgUrl = e.data;            
-            vote.attr("data-content","<span class=\"text-center\"><img height="50px" width="50px" src='"+imgUrl+"' /></span>");
+            vote.attr("data-content","<span class=\"text-center\"><img height=\"50px\" width=\"50px\" src='"+imgUrl+"' /></span>");
         });
     });
 
