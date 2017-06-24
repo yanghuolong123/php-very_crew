85ddc73ce7c85ada50dccd9012c6213ad3210543
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2017-06-24 04:56:29 +0000

    yii

--- frontend/config/memcache.php
+++ frontend/config/memcache.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    'class' => 'yii\caching\MemCache',
+    //'class' => 'yii\caching\MemCache',
+    'class' => '\app\components\ext\ExtMemCache',
     //'useMemcached' => true,
     'servers' => [
         [
