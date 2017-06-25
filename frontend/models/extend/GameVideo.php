ac06b067dee60e92db5d9fdf7336326056791767
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2017-06-25 09:07:47 +0000

    yii

--- frontend/models/extend/GameVideo.php
+++ frontend/models/extend/GameVideo.php
@@ -16,13 +16,13 @@ class GameVideo extends \app\models\native\TblGameVideo {
 
         $collection = Yii::$app->mongodb->getCollection('game_vote_record');
         if (!empty($collection->findOne(['game_video_id' => $videoId, 'open_id' => $openId]))) {
-            return '亲，你已经对作品 <a href="'.Url::to(['/video/view', 'id'=>$videoId], TRUE).'">《' . $videoModel->title . '》</a>投票过了，感谢你的参与!';
+            return '亲，你已经对作品 <a href="'.Url::to(['/video/view', 'id'=>$videoId], TRUE).'">《' . $videoModel->title . '》</a>投过票了，感谢你的参与!';
         }
 
         self::updateAllCounters(['votes' => 1], ['video_id' => $videoId]);
         $collection->insert(['game_video_id' => $videoId, 'open_id' => $openId, 'time' => DATETIME]);
 
-        return '亲，感谢你对作品 <a href="'.Url::to(['/video/view', 'id'=>$videoId], TRUE).'">《' . $videoModel->title . '》</a>投票了一票!';
+        return '亲，感谢你投票给作品 <a href="'.Url::to(['/video/view', 'id'=>$videoId], TRUE).'">《' . $videoModel->title . '》</a>!';
     }
 
 }
