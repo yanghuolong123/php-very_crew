4b6d7633662c404af47825fceb0ee6f4eaad9a4b
yanghuolong <yanghuolong@7c752d58-f4a2-48f2-83c4-a7bf3b1aa404>
2017-08-11 08:49:10 +0000

    yii

--- frontend/controllers/VideoController.php
+++ frontend/controllers/VideoController.php
@@ -60,7 +60,7 @@ class VideoController extends \app\util\BaseController {
 
         $otherWorks = Video::find()->where(['uid' => $model->uid])->andWhere(['in', 'status', [1, 2]])->andWhere(['<>', 'id', $id])->orderBy('id desc')->limit(8)->all();
         $members = VideoUser::findAll(['video_id' => $id, 'status' => 0]);
-        $rewardList = \app\models\extend\Order::findAll(['product_id'=>$id, 'status'=>0]);
+        $rewardList = \app\models\extend\Order::findAll(['product_id'=>$id, 'status'=>1]);
 
         return $this->render('view', [
                     'model' => $model,
