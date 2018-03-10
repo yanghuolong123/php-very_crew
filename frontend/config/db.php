929b5d7b84ba1a97b94468818ea8dbba7155bc73
杨活龙 <yanghuolong@zhisland.com>
2018-03-10 13:03:28 +0800

    db config

--- frontend/config/db.php
+++ frontend/config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=zd_very_crew',
-    'username' => 'feichangjuzu',
-    'password' => 'yhl-feichangjuzu',
+    'username' => 'root',
+    'password' => '123456',
     'charset' => 'utf8',
 ];
